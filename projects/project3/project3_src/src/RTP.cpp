e1d23f2b48d0dcf52769d4267d05125c07c3992d
Santi <sp92@rice.edu>
2023-09-19 18:47:19 +0000

    added project files

---
+++ projects/project3/project3_src/src/RTP.cpp
@@ -0,0 +1,17 @@
+///////////////////////////////////////
+// COMP/ELEC/MECH 450/550
+// Project 3
+// Authors: Santi Parra-Vargas, Eli Case
+//////////////////////////////////////
+
+/* 
+Goal of Project 3: Implement RTP as described
+
+Most recent changes: 
+Santi -- added the files to the repo, started looking at project details. 9/19/23 1:45pm
+
+*/
+
+#include "RTP.h"
+
+// TODO: Implement RTP as described
